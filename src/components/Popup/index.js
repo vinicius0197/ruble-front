cc9c473cb94016ce4152db97361705c962ded687
Vinicius <viniciuscosta0197@gmail.com>
2019-03-30 23:42:24 -0300

    added controlled components to popup window

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 
 export default class AccountPopup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      account_type: 'checking',
+      account_name: '',
+      account_balance: null
+    }
+  }
+
+  handleAccountType = (event) => {
+    this.setState({ account_type: event.target.value });
+  }
+
+  handleNameChange = (event) => {
+    this.setState({ account_name: event.target.value });
+  }
+
+  handleBalanceChange = (event) => {
+    this.setState({ account_balance: event.target.value });
+  }
+
+
   render() {
     return(
       <div className="popup" id="popup">
@@ -21,7 +43,11 @@ export default class AccountPopup extends Component {
             <form>
               <div className="popup__options--account-input">
                 <span><b>Que tipo de conta você quer adicionar?</b></span>
-                <select className="options__select">
+                <select
+                  className="options__select"
+                  value={this.state.account_type}
+                  onChange={this.handleAccountType}
+                >
                   <option value="checking">Conta Corrente</option>
                   <option value="savings">Conta Poupança</option>
                   <option value="credit">Cartão de Crédito</option>
@@ -30,12 +56,22 @@ export default class AccountPopup extends Component {
 
               <div className="popup__options--account-input ">
                 <span><b>Dê um nome para a conta:</b></span>
-                <input className="popup__text-input" type="text"/>
+                <input
+                  className="popup__text-input"
+                  type="text"
+                  value={this.state.account_name}
+                  onChange={this.handleNameChange}
+                />
               </div>
 
               <div className="popup__options--account-input">
                 <span><b>Qual o balanço atual da conta?</b></span>
-                <input className="popup__text-input" type="text"/>
+                <input
+                  className="popup__text-input"
+                  type="text"
+                  value={this.state.account_balance}
+                  onChange={this.handleBalanceChange}
+                />
               </div>
 
               <input type="submit" value="Enviar" className="popup__send-button"/>
