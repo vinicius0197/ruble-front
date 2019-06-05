7774272aca06f1df31c89e24efdd7b3901b0af43
vinicius <viniciuscosta0197@gmail.com>
2019-06-05 10:10:34 -0300

    adding transaction page basic styling

--- src/components/Transaction/index.js
+++ src/components/Transaction/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./index.css";
 
 export default class Transaction extends Component {
   constructor(props) {
@@ -7,7 +8,22 @@ export default class Transaction extends Component {
   render() {
     console.log(this.props.account);
     return(
-      <h1>Transactions</h1>
+      <div className="account">
+        <div className="account__header">
+          <div className="account__details">
+            <div className="account__name">
+              <span className="account__text">Conta</span>
+            </div>
+            <div className="account__balance">
+              <span>Balanço</span>
+              <div className="account__balance-value">R$ 25.000,00</div>
+            </div>
+          </div>
+          <button className="account__reconcile">Reconciliar Conta</button>
+        </div>
+        <div className="account__buttons">Buttons</div>
+        <div className="account__listing">Listing</div>
+      </div>
     );
   }
 }
