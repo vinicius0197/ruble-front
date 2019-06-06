f585eadf48668e39884ade03cc5fc85a47567e6a
Vinicius <viniciuscosta0197@gmail.com>
2019-06-05 21:11:03 -0300

    adding basic transactions styling

--- src/components/Transaction/index.js
+++ src/components/Transaction/index.js
@@ -19,10 +19,32 @@ export default class Transaction extends Component {
               <div className="account__balance-value">R$ 25.000,00</div>
             </div>
           </div>
-          <button className="account__reconcile">Reconciliar Conta</button>
+          <div className="account__reconcile">Reconciliar Conta</div>
         </div>
-        <div className="account__buttons">Buttons</div>
-        <div className="account__listing">Listing</div>
+        <div className="account__buttons">
+          <div className="account__add-transaction">
+            <span>+ Adicionar Transação</span>
+          </div>
+        </div>
+        <div className="account__listing">
+          <ul className="table__header">
+            <input className="table__header-select" type="checkbox" />
+            <li className="table__header-date">Data</li>
+            <li className="table__header-category">Categoria</li>
+            <li className="table__header-memo">Memo</li>
+            <li className="table__header-outflow">Outflow</li>
+            <li className="table__header-inflow">Inflow</li>
+          </ul>
+
+          <ul className="table__listing">
+            <input type="checkbox" className="table__listing-select"/>
+            <li className="table__listing-date">20/04/2019</li>
+            <li className="table__listing-category">Comida</li>
+            <li className="table__listing-memo">Compras do mês</li>
+            <li className="table__listing-outflow">R$ 220</li>
+            <li className="table__listing-inflow"></li>
+          </ul>
+        </div>
       </div>
     );
   }
